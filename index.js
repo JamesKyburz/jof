4a44e34db1989e8cd9e4da21452c2832d1e0eb94
James Kyburz <james.kyburz@gmail.com>
2016-05-01 16:06:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var http = require('http')
+var spawn = require('cross-spawn')
+var concat = require('concat-stream')
+var server = http.createServer()
+var port = process.env.PORT || 1234
+
+module.exports = jof
+
+function jof (opt) {
+  server.on('request', (q, r) => {
+    var pending = Object.keys(opt.client).length
+    Object.keys(opt.client).forEach((url) => {
+      if (typeof opt.client[url] === 'string') return bundled()
+      var ps = spawn('browserify', ['-'].concat(opt.browserify || []))
+      var js = opt.client[url].toString().split('\n').slice(1, -1).join('\n')
+      ps.stdin.write(js)
+      ps.stdin.end()
+      ps.stdout.pipe(concat((js) => {
+        opt.client[url] = js.toString()
+        bundled()
+      }))
+    })
+    function bundled () {
+      if (!--pending) {
+        var match = false
+        Object.keys(opt.routes).forEach((url) => {
+          if (match) return
+          if (q.url === url) {
+            r.setHeader('content-type', 'text/html')
+            opt.routes[url](opt, q, r)
+            match = true
+          }
+        })
+        if (!match) {
+          r.writeHead(404)
+          r.end()
+        }
+      }
+    }
+  })
+}
+
+jof.html = html
+
+function html (js) {
+  return `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>jof</title>
+</head>
+<body>
+  <script>${js}</script>
+</body>
+</html>
+`
+}
+server.listen(port, console.log.bind(console, 'open http://localhost:%s', port))
---
+++ readme.markdown
@@ -0,0 +1,7 @@
+# jof
+
+:zap: Just one file :zap:
+
+For quick prototyping write server and client code in a single file
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
